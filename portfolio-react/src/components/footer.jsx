d9159e567ec31bdf91cc4938f7ea66bd8d193b3e
Mark Anthony <touya1313@hotmail.com>
2023-10-20 00:22:51 -0600

    having a great deal of trouble getting what I have to display on screen. Its just a blank page

--- portfolio-react/src/components/footer.jsx
+++ portfolio-react/src/components/footer.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from 'react'
 
 function Footer() {
     return (
         <footer>
             <div className="social-links">
                 <a href="https://github.com/Gera1313" target="_blank" rel="noopener noreferrer">
-                {/* <i className="fab fa-github"></i> took out FontAwesome */}
                 </a>
                 <a href="https://www.linkedin.com/in/gerardoperez13/" target="_blank" rel="noopener noreferrer">
-                    {/* <i className="fab fa-linkedin"></i> took out FontAwesome */}
                 </a>
             </div>
         </footer>
