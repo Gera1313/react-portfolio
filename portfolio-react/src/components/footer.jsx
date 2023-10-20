c0663db4c20c90b8664d56a9245316a3b1375113
Mark Anthony <touya1313@hotmail.com>
2023-10-20 00:08:20 -0600

    took out fontawesome

--- portfolio-react/src/components/footer.jsx
+++ portfolio-react/src/components/footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
         <footer>
             <div className="social-links">
                 <a href="https://github.com/Gera1313" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-github"></i>
+                {/* <i className="fab fa-github"></i> took out FontAwesome */}
                 </a>
                 <a href="https://www.linkedin.com/in/gerardoperez13/" target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-linkedin"></i>
+                    {/* <i className="fab fa-linkedin"></i> took out FontAwesome */}
                 </a>
             </div>
         </footer>
