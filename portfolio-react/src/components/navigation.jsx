eb92ddbb5e538c06161eee001b88abe5be7adbcd
Mark Anthony <touya1313@hotmail.com>
2023-10-25 03:59:50 -0600

    added CSS style and it applies to all, but i need to move it to the far right

--- portfolio-react/src/components/navigation.jsx
+++ portfolio-react/src/components/navigation.jsx
@@ -3,20 +3,26 @@ import { Link } from 'react-router-dom';
 function Navigation() {
     return (
     <nav className="navigation">
-        <ul>
-            <li>
-                <Link to="/">About Me</Link>
-            </li>
-            <li>
-                <Link to="portfolio">Portfolio</Link>
-            </li>
-            <li>
-                <Link to="contact">Contact</Link>
-            </li>
-            <li>
-                <Link to="resume">Resume</Link>
-            </li>
-        </ul>
+        <div className="nav-link">
+            <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+                About Me
+            </Link>
+        </div>
+        <div className="nav-link">
+            <Link to="/portfolio" className={location.pathname === '/portfolio' ? 'active' : ''}>
+                Portfolio
+            </Link>
+        </div>
+        <div className="nav-link">
+            <Link to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>
+                Contact
+            </Link>
+        </div>
+        <div className="nav-link">
+            <Link to="/resume" className={location.pathname === '/resume' ? 'active' : ''}>
+                Resume
+            </Link>
+        </div>
     </nav>
     );
 }
