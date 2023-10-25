87ca1517d0fb3dd6e8c6622d5d2e924b5de628fb
Mark Anthony <touya1313@hotmail.com>
2023-10-24 23:24:24 -0600

    footer icons now work and they appear in all pages

--- portfolio-react/src/components/footer.jsx
+++ portfolio-react/src/components/footer.jsx
@@ -2,11 +2,11 @@ function Footer() {
     return (
         <footer>
             <div className="social-links">
-                <h2>GitHub</h2>
+                {/* <h2>GitHub</h2> */}
                 <a href="https://github.com/Gera1313" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-github"></i>
                 </a>
-                <h2>LinkedIn</h2>
+                {/* <h2>LinkedIn</h2> */}
                 <a href="https://www.linkedin.com/in/gerardoperez13/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-linkedin"></i>
                 </a>
