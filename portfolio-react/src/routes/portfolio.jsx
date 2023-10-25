c7fc2a4dbdff7abad5fb7a5ffd40378d15abb81b
Mark Anthony <touya1313@hotmail.com>
2023-10-25 15:35:23 -0600

    added images

--- portfolio-react/src/routes/portfolio.jsx
+++ portfolio-react/src/routes/portfolio.jsx
@@ -2,37 +2,37 @@ function Portfolio() {
   const projects = [
     {
       title: 'Vroomu',
-      image: "./portfolio-react/assets/images/IMG_9911.jpeg",
+      image: "/assets/images/project1.png",
       githubLink: 'https://github.com/Gera1313/Vroomu-A-Vehicle-Service-Guide',
       deployedLink: 'https://gera1313.github.io/Vroomu-A-Vehicle-Service-Guide/',
     },
     {
       title: 'Logo Generator',
-      image: './portfolio-react/assets/images/IMG_9911.jpeg',
+      image: '/assets/images/project2.png',
       githubLink: 'https://github.com/Gera1313/10-logo-svg',
       deployedLink: 'https://youtu.be/yTYZvp4YYHU',
     },
     {
       title: 'ReadMe Guide Generator',
-      image: './portfolio-react/assets/images/IMG_9911.jpeg',
+      image: '/assets/images/project3.png',
       githubLink: 'https://github.com/Gera1313/9-readme-gen',
       deployedLink: 'https://youtu.be/NOzqGhCcUIk',
     },
     {
       title: 'First Portfolio',
-      image: './portfolio-react/assets/images/IMG_9911.jpeg',
+      image: '/assets/images/project6.png',
       githubLink: 'https://github.com/Gera1313/2-Gerardo-Portfolio',
       deployedLink: 'https://gera1313.github.io/2-Gerardo-Portfolio/',
     },
     {
       title: 'Employee Tracker',
-      image: './portfolio-react/assets/images/IMG_9911.jpeg',
+      image: '/assets/images/project5.png',
       githubLink: 'https://github.com/Gera1313/12-employee-tracker',
       deployedLink: 'https://youtu.be/MP4zhBCbCsM?si=K5fLm4J1NEU6N-sD',
     },
     {
       title: 'Project 6 here',
-      image: './portfolio-react/assets/images/IMG_9911.jpeg',
+      image: '/assets/images/project4.png',
       githubLink: 'https://github.com/Gera1313',
       deployedLink: 'https://gera1313.github.io/2-Gerardo-Portfolio/',
     },
